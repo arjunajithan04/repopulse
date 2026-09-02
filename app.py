6f53a1c205fbe65cf1a8b8e4a65bc1218a5a9ae2
arjunajithan04 <arjunajithan04@users.noreply.github.com>
2026-09-02 09:35:58 +0530

    Pages Modification

--- app.py
+++ app.py
@@ -258,12 +258,12 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# pages = {
-#     "Dashboard": dashboard_page,
-#     "Repository": repository_page,
-#     "Contributors": contributors_page,
-#     "Code Insights": code_insights_page,
-# }
+pages = {
+    "Dashboard": dashboard_page,
+    "Repository": repository_page,
+    "Contributors": contributors_page,
+    "Code Insights": code_insights_page,
+}
 
 with st.sidebar:
     st.markdown('<div class="sidebar-header">RepoPulse</div>', unsafe_allow_html=True)
